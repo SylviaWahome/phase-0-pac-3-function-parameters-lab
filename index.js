977ca17891fab3c72d8746b5e393eb352822bb22
Shee-001 <sylviawanjiru804@gmail.com>
2025-05-27 00:27:24 +0300

    Function Parameters

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// Function with one parameter
 function introduction(name) {
-    return `Hi, my name is ${name}.`;
-  }
-  
-  function introductionWithLanguage(name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }
-  
-  function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }
+  return `Hi, my name is ${name}.`;
+}
+
+// Function with two parameters
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// Function with default parameter
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+// Don't forget to export the functions if needed for tests
+module.exports = {
+  introduction,
+  introductionWithLanguage,
+  introductionWithLanguageOptional,
+};
